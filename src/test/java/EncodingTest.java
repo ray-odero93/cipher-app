b93240cb0aaf97e042e69c172b54bda99503f129
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 12:44:17 +0300

    Successful test for instances of creating new Encoding class.

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class EncodingTest {
 
     @Test
-    public void encode() {
+    public void newEncoding_instantiatesCorrectly() {
+        Encoding testEncoding = new Encoding("raymond", '1');
+        assertEquals(true, testEncoding instanceof Encoding);
     }
 }
