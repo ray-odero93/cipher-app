ea6ede1b97195e6220e5f8bc0293eb9f40b0914b
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 12:45:14 +0300

    Add constructor for Encoding class.

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,11 +1,5 @@
 public class Encoding {
-    public static String encode(String message, int key) {
-        String encodedText = "";
-        key %= 26;
 
-        int length = message.length();
-
-
-        return null;
+    public Encoding(String message, int key){
     }
 }
