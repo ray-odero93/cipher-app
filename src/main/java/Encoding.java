eabc44d161b38a6a1058580f78c55cb18770315c
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 18:55:47 +0300

    Include any changes that might have been missed.

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -3,9 +3,9 @@ public class Encoding {
     private int key;
     private String message;
 
-    public Encoding(String message, int key) {
-        this.key = key;
-        this.message = message;
+    public Encoding(String yourMessage, int yourKey) {
+        this.key = yourKey;
+        this.message = yourMessage;
     }
 
     public int getKey() {
@@ -16,11 +16,41 @@ public class Encoding {
       return this.message;
     }
 
-    public String encode() {
-        String encoded;
-        key %= 26;
+    public String encrypt(String message, int key) {
+        String encryptedMessage = " ";
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        message = message.toUpperCase();
+         int charPosition;
+         int keyValue;
+         char encryptedValue;
 
+         for (int i = 0; i < message.length(); i++) {
+             switch (message.charAt(i)) {
+                 case ' ': encryptedMessage += ' ';
+                 break;
+                 case '.': encryptedMessage += '.';
+                     break;
+                 case ',': encryptedMessage += ',';
+                     break;
+                 case '!': encryptedMessage += '!';
+                     break;
+                 case '?': encryptedMessage += '?';
+                     break;
 
-        return null;
+                 default: charPosition = letters.indexOf(message.charAt(i));
+
+                 keyValue = (key + charPosition) % 26;
+
+                 if (keyValue <  0) {
+                     keyValue = letters.length() + keyValue;
+                 }
+
+                 encryptedValue = letters.charAt(keyValue);
+                 encryptedMessage += encryptedValue;
+                 break;
+             }
+         }
+
+        return encryptedMessage;
     }
 }
--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -4,22 +4,29 @@ import static org.junit.Assert.assertEquals;
 
 public class EncodingTest {
 
-    Encoding testEncoding = new Encoding("raymond",'1');;
-
-
     @Test
-    public void newEncoding_instantiatesCorrectly() {
-        Encoding testEncoding = new Encoding("raymond", '1');
+    public void newEncoding_instantiateCreation() {
+        Encoding testEncoding = new Encoding("Dutch",'2');
         assertEquals(true, testEncoding instanceof Encoding);
     }
 
     @Test
-    public void checkTextInputted() {
-        assertEquals("globe", testEncoding.getInputText());
+    public void checkKeySaved(){
+        Encoding testEncoding = new Encoding("Dutch", '2');
+        assertEquals(2, testEncoding.getKey());
     }
 
     @Test
-    public void checkInputtedKey() {
-        assertEquals(2, testEncoding.getKey());
+    public void checkInputSaved() {
+        Encoding testEncoding = new Encoding("Dutch",'2');
+        assertEquals("Dutch", testEncoding.getInputText());
     }
+
+    @Test
+    public void validEncoding() {
+        Encoding testEncoding = new Encoding(" ", ' ' );
+        assertEquals(" ", testEncoding.encrypt(" ", ' '));
+    }
+
+
 }
