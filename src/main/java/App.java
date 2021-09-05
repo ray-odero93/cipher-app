5bbc82c9c1ab75de933020b5557d6d752128fc4c
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 10:49:56 +0300

    Add prompt asking user to enter a  message. Declare a variable that will store captured text input.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
+import javax.crypto.spec.PSource;
 import java.util.Scanner;
 
 public class App {
-    Scanner inputText = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner inputText = new Scanner(System.in);
 
-
+        System.out.println("Enter your message:");
+        String message = inputText.nextLine();
+    }
 }
