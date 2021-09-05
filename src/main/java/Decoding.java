d3854aff6e772980c4fc586e977613ffd453776a
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 19:13:44 +0300

    Define method to get value of user inputted key.

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,2 +1,13 @@
 public class Decoding {
+    private int key;
+    private String message;
+
+    public Decoding(String message, int key){
+        this.key = key;
+        this.message = message;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
 }
