927a115c23d8982af591e31a1a22aab7b7388254
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 13:04:38 +0300

    Successful test on type of input given by user, string.

--- src/test/java/EncodingTest.java
+++ src/test/java/EncodingTest.java
@@ -4,9 +4,17 @@ import static org.junit.Assert.assertEquals;
 
 public class EncodingTest {
 
+    Encoding testEncoding = new Encoding("raymond",'1');;
+
+
     @Test
     public void newEncoding_instantiatesCorrectly() {
         Encoding testEncoding = new Encoding("raymond", '1');
         assertEquals(true, testEncoding instanceof Encoding);
+    }
+
+    @Test
+    public void checkTextInputted() {
+        assertEquals("globe", testEncoding.getInputText());
     }
 }
