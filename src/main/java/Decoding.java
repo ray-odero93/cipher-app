6947b4f0d87db9c28c804712ef94a8d3ef50b1b2
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 19:43:26 +0300

    Define function to decrypt encrypted messages.

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -2,9 +2,9 @@ public class Decoding {
     private int key;
     private String message;
 
-    public Decoding(String message, int key){
+    public Decoding(String encryptedMessage, int key){
         this.key = key;
-        this.message = message;
+        this.message = encryptedMessage;
     }
 
     public int getKey() {
@@ -13,5 +13,40 @@ public class Decoding {
 
     public String getInputtedText() {
         return this.message;
+    }
+
+    public String decrypt( String encryptedMessage, int key) {
+        String originalMessage = " ";
+        encryptedMessage = encryptedMessage.toUpperCase();
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int charPosition;
+        int keyValue;
+        char initialValue;
+
+        for (int i = 0; i <encryptedMessage.length(); i++) {
+            switch (encryptedMessage.charAt(i)) {
+                case ' ': originalMessage += ' ';
+                break;
+                case '.': originalMessage += '.';
+                    break;
+                case ',': originalMessage += ',';
+                    break;
+                case '!': originalMessage += '!';
+                    break;
+                case '?': originalMessage += '?';
+                    break;
+
+                default: charPosition = letters.indexOf(encryptedMessage.charAt(i));
+                keyValue = (charPosition - key) % 26;
+
+                if (keyValue < 0) {
+                    keyValue = letters.length() + keyValue;
+                }
+                initialValue = letters.charAt(keyValue);
+                originalMessage += initialValue;
+                break;
+            }
+        }
+        return originalMessage;
     }
 }
