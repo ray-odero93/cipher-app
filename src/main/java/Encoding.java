a61ced2f6f6ef1221eb33db62a99fff5af37af29
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 15:38:56 +0300

    Define encrypting function.

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,5 +1,26 @@
 public class Encoding {
 
-    public Encoding(String message, int key){
+    private int key;
+    private String message;
+
+    public Encoding(String message, int key) {
+        this.key = key;
+        this.message = message;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+
+    public String getInputText() {
+      return this.message;
+    }
+
+    public String encode() {
+        String encoded;
+        key %= 26;
+
+
+        return null;
     }
 }
