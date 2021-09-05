48ce0696f8a3d3d454cb260c6d2d34b1315c74ca
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-09-05 11:00:37 +0300

    Declare variables to contain encoded text, shift value and size of array.

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,2 +1,11 @@
 public class Encoding {
+    public static String encode(String message, int key) {
+        String encodedText = "";
+        key %= 26;
+
+        int length = message.length();
+
+
+        return null;
+    }
 }
